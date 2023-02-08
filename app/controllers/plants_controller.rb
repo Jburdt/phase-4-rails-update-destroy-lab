ccc83de56798fa93594dcdb253f868fcb896f5fc
Josh Burd <Jburdt@gmail.com>
2023-02-08 13:58:12 -0500

    commit tree 78e91fe10a0bbe5ee21dc0395309a71544b2fcde

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,7 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  before_action :find_plant, only: [ :show, :update, :destroy ]
 
   # GET /plants
   def index
@@ -8,8 +11,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: @plant
   end
 
   # POST /plants
@@ -18,9 +20,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/:id
+  def update
+    @plant.update(plant_params)
+    render json: @plant
+  end
+
+  def destroy
+    @plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+      @plant = Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 end
